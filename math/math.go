80808cf0d39d14ae5ccbe5aca8504171554e1904
sprappcom <156875069+sprappcom@users.noreply.github.com>
2024-08-11 00:07:53 -0400

    Update math.go

--- math/math.go
+++ math/math.go
@@ -1,21 +1,30 @@
 package cxutilmath
 
 import (
-	"golang.org/x/exp/constraints"
+    "golang.org/x/exp/constraints"
 )
 
 // Max returns the greater of two values of an ordered type.
 func Max[T constraints.Ordered](a, b T) T {
-	if a > b {
-		return a
-	}
-	return b
+    if a > b {
+        return a
+    }
+    return b
 }
 
 // Min returns the lesser of two values of an ordered type.
 func Min[T constraints.Ordered](a, b T) T {
-	if a < b {
-		return a
-	}
-	return b
+    if a < b {
+        return a
+    }
+    return b
+}
+
+// IsWithinDifference checks if the difference between a and b is not more than c.
+// It is generic over any ordered type.
+func IsWithinDifference[T constraints.Ordered](a, b, c T) bool {
+    if a > b {
+        return a-b <= c
+    }
+    return b-a <= c
 }
